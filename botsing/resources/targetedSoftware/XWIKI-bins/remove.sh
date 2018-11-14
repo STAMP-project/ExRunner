f1c2d6b9fe6dfeed4d6509a7ef15dba7e61f135c
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-11-14 16:41:32 +0100

    Remove extra jar files

--- botsing/resources/targetedSoftware/XWIKI-bins/remove.sh
+++
@@ -1,6 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-find $(pwd) -maxdepth 20 -type f -name '*-version.jar' | while read candidates; do
-  rm $candidates
-  echo "$candidates Removed!"
-done
